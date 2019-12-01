a8f171b72a415c289f490420dfa689200e3015cf
Tabish Rashid <tabzraz@gmail.com>
2019-12-01 20:28:23 +0000

    Updating config files to SMAC v1 settings. The biggest change is using the episode runner and training for 2mil timesteps.
    Updating QMIX to allow for 2 layers in the hypernetwork.
    
    Adding QTran implementation.

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -1,7 +1,9 @@
 from .q_learner import QLearner
 from .coma_learner import COMALearner
+from .qtran_learner import QLearner as QTranLearner
 
 REGISTRY = {}
 
 REGISTRY["q_learner"] = QLearner
 REGISTRY["coma_learner"] = COMALearner
+REGISTRY["qtran_learnet"] = QTranLearner

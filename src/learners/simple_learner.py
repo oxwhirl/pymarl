06f84e19e71d291a23f8d11fac5c37bd4819265d
Sina Masoud-Ansari <sina.masoud.ansari@gmail.com>
2020-06-05 02:03:23 +0000

    integrate obs model and state model learning

--- src/learners/simple_learner.py
+++ src/learners/simple_learner.py
@@ -1,7 +1,7 @@
 import time
 import torch
 import torch.nn.functional as F
-from modules.models.simple import SimPLeStateModel
+from modules.models.simple import SimPLeStateModel, SimPLeObservationModel
 import numpy as np
 import random
 
@@ -12,6 +12,7 @@ class SimPLeLearner:
         self.params = list(mac.parameters())
         self.args = args
         self.logger = logger
+        self.device = self.args.device
 
         # model takes current state and joint-action and predicts next-state, reward and termination
         self.action_size = args.n_actions * args.n_agents
@@ -19,9 +20,15 @@ class SimPLeLearner:
         self.reward_size = scheme["reward"]["vshape"][0]
         self.term_size = scheme["terminated"]["vshape"][0]
 
-        self.hidden_size = args.model_hidden_dim
-        self.state_model = SimPLeStateModel(self.hidden_size, self.state_size, self.action_size,
+        self.state_model = SimPLeStateModel(args.state_model_hidden_dim, self.state_size, self.action_size,
                                             reward_size=self.reward_size, term_size=self.term_size)
+
+        obs_model_input_size = self.state_size + self.term_size
+        if args.obs_model_include_last_action:
+            obs_model_input_size += self.action_size
+        obs_aa_size = self.args.n_agents * (scheme["avail_actions"]["vshape"][0] + scheme["obs"]["vshape"])
+
+        self.obs_model = SimPLeObservationModel(obs_model_input_size, obs_aa_size, args.obs_model_hidden_dim)
 
     def train_test_split(self, indices, test_ratio=0.1, shuffle=True):
 
@@ -35,10 +42,26 @@ class SimPLeLearner:
 
         return train_indices, test_indices
 
-    def get_episode_state(self, ep):
+    def get_episode_vars(self, ep):
 
-        state = ep["state"][:, :-1]
-        reward = ep["reward"][:, :-1]
+        # per-agent quantities
+        obs = ep["obs"][:, :-1, :]  # observations
+        aa = ep["avail_actions"][:, :-1, :].float()  # available actions
+        actions = ep["actions_onehot"][:, :-1, :]  # actions taken
+
+        # flatten per-agent quantities
+        nbatch, ntimesteps, _, _ = obs.size()
+        obs = obs.view((nbatch, ntimesteps, -1))
+        aa = aa.view((nbatch, ntimesteps, -1))
+        actions = actions.view(nbatch, ntimesteps, -1)
+
+        # state
+        state = ep["state"][:, :-1, :]
+        if self.args.env_args["state_last_action"]:
+            state = state[:, :, :self.state_size]
+
+        # reward
+        reward = ep["reward"][:, :-1, :]
 
         # termination signal
         terminated = ep["terminated"][:, :-1].float()
@@ -46,160 +69,224 @@ class SimPLeLearner:
         term_signal = torch.ones_like(terminated)
         term_signal[:, :term_idx, :] = 0
 
-        # mask for active timesteps
+        # mask for active timesteps (except for term_signal which is always valid)
         mask = torch.ones_like(terminated)
         mask[:, term_idx + 1:, :] = 0
 
-        if self.args.env_args["state_last_action"]:
-            x = state  # input state includes joint-action
-            y = state[:, :, :self.state_size]  # output state excludes joint-action
-        else:
-            x = state  # state does not include joint-action
-            y = state
+        obs *= mask
+        aa *= mask
+        actions *= mask
+        reward *= mask
+        state *= mask
 
-            # append joint-action to input
-            actions = ep["actions_onehot"][:, :-1]
-            n_episodes, n_timesteps, _ = state.size()
-            actions = actions.view(n_episodes, n_timesteps, -1)
-            x = torch.cat((state, actions), -1)
+        return state, actions, reward, term_signal, obs, aa, mask
 
-        # append reward and termination signal to output
-        y = torch.cat((y, reward, term_signal), -1)
+    def get_batch(self, episodes, batch_size, use_mask=False):
+        # TOOD: refactor to use list of ids passed to replay buffer
+        bs = min(batch_size, len(episodes))
+        batch = random.sample(episodes, bs)
+        props = [torch.cat(t) for t in zip(*batch)]
+        if use_mask:
+            mask = props[-1]
+            idx = int(mask.sum(1).max().item())
+            props = [x[:, :idx, :] for x in props]
+        return props
 
-        # mask input and output by valid timesteps
-        x *= mask
-        y[:, :, :-1] *= mask  # ignore masking term_signal
+    def get_state_model_input_output(self, state, actions, reward, term_signal, obs, aa, mask):
+        # inputs and outputs are offset by 1 timestep for 1-step-ahead prediction
+        s = state[:, :-1, :]
+        a = actions[:, :-1, :]
+        y = torch.cat((state, reward, term_signal), dim=-1)
+        y = y[:, 1:, :]
+        return s, a, y
 
-        # slice to get input at time t and supervised signal at t+1
-        x = x[:, :-1, :]  # skip the last timestep
-        y = y[:, 1:, :]  # skip the first timestep
+    def run_state_model(self, state, actions, output_size):
 
-        return x, y, mask
+        bs, steps, state_size = state.size()
+        ht_ct = self.state_model.init_hidden(bs, self.device)
+        yp = torch.zeros(bs, steps, output_size).to(self.device)
 
-    def generate_batch(self, episodes, batch_size, mask=False):
-        batch = random.sample(episodes, min(batch_size, len(episodes)))
-        x, y, m = (torch.cat(t) for t in zip(*batch))
-        if mask:
-            mask_index = int(torch.sum(m, 1).max().item())  # exclude post-terminal timesteps
-            x = x[:, :mask_index, :]
-            y = y[:, :mask_index, :]
-        return x.to(self.args.device), y.to(self.args.device)
+        st = state[:, 0, :]  # initial state
+        for t in range(0, steps):
+            at = actions[:, t, :]
+            xt = torch.cat((st, at), dim=-1)
+
+            yt, ht_ct = self.state_model(xt, ht_ct)
+            yp[:, t, :] = yt
+
+            st = yt[:, :state_size]
+        return yp
+
+    def get_obs_model_input_output(self, state, actions, reward, term_signal, obs, aa, mask):
+        y = torch.cat((obs, aa), dim=-1)
+        return state, actions, term_signal, y
+
+    def train_state_model(self, train_episodes, test_episodes):
+        # model learning parameters
+        lr = self.args.state_model_learning_rate
+        grad_clip = self.args.state_model_grad_clip_norm
+        batch_size = self.args.state_model_train_batch_size
+        batch_size = min(batch_size, len(test_episodes))
+        state_model_output_size = self.state_size + self.reward_size + self.term_size
+        optimizer = torch.optim.Adam(self.state_model.parameters(), lr=lr)
+        epochs = self.args.state_model_train_epochs
+        log_epochs = self.args.state_model_train_log_epochs
+        use_mask = False # learning a termination signal is easier with unmasked input
+
+        # train state model
+        for e in range(epochs):
+
+            t_start = time.time()
+            self.state_model.train()
+            train_err = []
+
+            props = self.get_batch(train_episodes, batch_size, use_mask=use_mask)
+            state, actions, y = self.get_state_model_input_output(*props)
+            yp = self.run_state_model(state.to(self.device), actions.to(self.device), state_model_output_size)
+            optimizer.zero_grad()  # zero the gradient buffers
+            loss = F.mse_loss(yp, y.to(self.device))
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.state_model.parameters(), grad_clip)
+            optimizer.step()
+
+            train_err.append(loss.item())
+
+            self.state_model.eval()
+            val_err = []
+            with torch.no_grad():
+
+                props = self.get_batch(test_episodes, batch_size, use_mask=use_mask)
+                state, actions, y = self.get_state_model_input_output(*props)
+                yp = self.run_state_model(state.to(self.device), actions.to(self.device),  state_model_output_size)
+                val_err.append(F.mse_loss(yp, y.to(self.device)).item())
+
+            if (e + 1) % log_epochs == 0:
+                # report epoch losses
+                train_err = np.array(train_err)
+                val_err = np.array(val_err)
+                t_epoch = time.time() - t_start
+                print(f"epoch: {e + 1:<3}   train loss: mean {train_err.mean():.5f}, std: {train_err.std():.5f}   val loss: mean {val_err.mean():.5f}, std: {val_err.std():.5f}   time: {t_epoch:.2f} s")
+                train_err = []
+                val_err = []
+                # self.logger.console_logger.info(f"Model training epoch {i}")
+
+    def train_obs_model(self, train_episodes, test_episodes):
+        # observation model training
+        print(f"Observation Model Training ...")
+
+        # model learning parameters
+        lr = self.args.obs_model_learning_rate
+        grad_clip = self.args.obs_model_grad_clip_norm
+        batch_size = self.args.obs_model_train_batch_size
+        batch_size = min(batch_size, len(test_episodes))
+        state_model_output_size = self.state_size + self.reward_size + self.term_size
+        optimizer = torch.optim.Adam(self.obs_model.parameters(), lr=lr)
+        epochs = self.args.obs_model_train_epochs
+        log_epochs = self.args.obs_model_train_log_epochs
+        use_mask = self.args.obs_model_use_mask
+        optimizer = torch.optim.Adam(self.obs_model.parameters(), lr=lr)
+        train_err = []
+        val_err = []
+
+        self.state_model.eval()
+        for e in range(epochs):
+            t_start = time.time()
+            # use state model and real actions to generate synthetic episodes from real starts
+            with torch.no_grad():
+                props = self.get_batch(train_episodes, batch_size, use_mask=use_mask)
+                r_state, actions, term_signal, y = self.get_obs_model_input_output(*props)
+                m_state = self.run_state_model(r_state.to(self.device), actions.to(self.device),
+                                               state_model_output_size)
+                m_state = m_state[:, :-1, :r_state.size()[-1]]  # exclude reward and term_signal and final timestep
+
+                # prepend first real state to model generated states
+                s0 = torch.unsqueeze(r_state[:, 0, :], dim=1).to(self.device)
+                m_state = torch.cat((s0, m_state), dim=1)
+
+            # add last action
+            # actions are generated after the observation so the first one is null and the last one is omitted
+            last_action = actions[:, :-1, :].to(self.device)
+            a0 = torch.zeros_like(last_action[:, 0, :]).to(self.device)
+            a0 = torch.unsqueeze(a0, dim=1)
+            last_action = torch.cat((a0, last_action), dim=1)
+
+            # generate obs from states
+            self.obs_model.train()
+            if self.args.obs_model_include_last_action:
+                x = torch.cat((m_state, last_action, term_signal.to(self.device)), dim=-1)
+            else:
+                x = torch.cat((m_state, term_signal.to(self.device)), dim=-1)
+            yp = self.obs_model(x)
+
+            # train obs model
+            optimizer.zero_grad()
+            loss = F.mse_loss(yp, y.to(self.device))
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.obs_model.parameters(), grad_clip)
+            optimizer.step()
+            train_err.append(loss.item())
+
+            # validate obs model
+            with torch.no_grad():
+                props = self.get_batch(test_episodes, batch_size, use_mask=use_mask)
+                r_state, actions, term_signal, y = self.get_obs_model_input_output(*props)
+                m_state = self.run_state_model(r_state.to(self.device), actions.to(self.device),
+                                               state_model_output_size)
+                m_state = m_state[:, :-1, :r_state.size()[-1]]  # exclude reward and term_signal and final timestep
+
+                # prepend first real state to model generated states
+                s0 = torch.unsqueeze(r_state[:, 0, :], dim=1).to(self.device)
+                m_state = torch.cat((s0, m_state), dim=1)
+
+                # add last action
+                last_action = actions[:, :-1, :].to(self.device)
+                a0 = torch.zeros_like(last_action[:, 0, :]).to(self.device)
+                a0 = torch.unsqueeze(a0, dim=1)
+                last_action = torch.cat((a0, last_action), dim=1)
+
+                # generate obs from states
+                self.obs_model.eval()
+                if self.args.obs_model_include_last_action:
+                    x = torch.cat((m_state, last_action, term_signal.to(self.device)), dim=-1)
+                else:
+                    x = torch.cat((m_state, term_signal.to(self.device)), dim=-1)
+                yp = self.obs_model(x)
+                val_err.append(F.mse_loss(yp, y.to(self.device)).item())
+
+            if (e + 1) % log_epochs == 0:
+                # report epoch losses
+                train_err = np.array(train_err)
+                val_err = np.array(val_err)
+                t_epoch = time.time() - t_start
+                print(
+                    f"epoch: {e + 1:<3}   train loss: mean {train_err.mean():.5f}, std: {train_err.std():.5f}   val loss: mean {val_err.mean():.5f}, std: {val_err.std():.5f}   time: {t_epoch:.2f} s")
+                train_err = []
+                val_err = []
 
     def train(self, episode_buffer):
 
-        print(f"Starting model training with {episode_buffer.episodes_in_buffer} episodes")
+        print(f"Training with {episode_buffer.episodes_in_buffer} episodes")
+        print(f"Model Training ...")
 
         # generate training and test episode indices
         indices = list(range(0, episode_buffer.episodes_in_buffer))
         train_indices, test_indices = self.train_test_split(indices, test_ratio=0.1, shuffle=True)
 
         # extract episodes
-        train_episodes = [self.get_episode_state(episode_buffer[i]) for i in train_indices]
-        test_episodes = [self.get_episode_state(episode_buffer[i]) for i in train_indices]
+        train_episodes = [self.get_episode_vars(episode_buffer[i]) for i in train_indices]
+        test_episodes = [self.get_episode_vars(episode_buffer[i]) for i in train_indices]
 
-        # model learning parameters
-        lr = self.args.model_learning_rate
-        grad_clip = self.args.model_grad_clip_norm
-        batch_size = self.args.model_train_batch_size
-        batch_size = min(batch_size, len(test_episodes))
-        output_size = self.state_size + self.reward_size + self.term_size
-        optimizer = torch.optim.Adam(self.state_model.parameters(), lr=lr)
-        epochs = self.args.model_train_epochs
-        n_timesteps = self.args.episode_limit - 1
-        log_epochs = self.args.model_train_log_epochs
-        mask = False # learning a termination signal is easier with unmasked input
+        self.train_state_model(train_episodes, test_episodes)
+        self.train_obs_model(train_episodes, test_episodes)
 
-        train_err = []
-        val_err = []
-        for i in range(epochs):
-            t_start = time.time()
-            self.state_model.train()
 
-            x, y = self.generate_batch(train_episodes, batch_size, mask=mask)
 
-            bs = x.size()[0]
-            a = x[:, :, self.state_size:]
-            ht = torch.zeros(bs, self.hidden_size).to(self.args.device)
-            ct = torch.zeros(bs, self.hidden_size).to(self.args.device)
-            yp = torch.zeros(bs, n_timesteps, output_size).to(self.args.device)
-
-            st = x[:, 0, :self.state_size]  # initial state
-            for t in range(0, n_timesteps):
-                at = a[:, t, :]  # joint-action
-                xt = torch.cat((st, at), dim=-1)
-
-                yt, ht, ct = self.state_model(xt, ht, ct)
-                yp[:, t, :] = yt
-
-                st = yt[:, :self.state_size]
-
-            optimizer.zero_grad()   # zero the gradient buffers
-            loss = F.mse_loss(yp, y)
-            train_err.append(loss.item())
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(self.state_model.parameters(), grad_clip)
-            optimizer.step()
-
-            # validate
-            with torch.no_grad():
-                self.state_model.eval()
-                x, y = self.generate_batch(test_episodes, batch_size, mask=False)
-
-                bs = x.size()[0]
-                a = x[:, :, self.state_size:]
-                ht = torch.zeros(bs, self.hidden_size).to(self.args.device)
-                ct = torch.zeros(bs, self.hidden_size).to(self.args.device)
-                yp = torch.zeros(bs, n_timesteps, output_size).to(self.args.device)
-
-                st = x[:, 0, :self.state_size]  # initial state
-                for t in range(0, n_timesteps):
-                    at = a[:, t, :]  # joint-action
-                    xt = torch.cat((st, at), dim=-1)
-
-                    yt, ht, ct = self.state_model(xt, ht, ct)
-                    yp[:, t, :] = yt
-
-                    st = yt[:, :self.state_size]
-
-                val_loss = F.mse_loss(yp, y)
-                val_err.append(val_loss.item())
-                t_epoch = time.time() - t_start
-
-            if (i+1) % log_epochs == 0:
-                train_err = np.array(train_err)
-                val_err = np.array(val_err)
-                print(f"epoch: {i + 1:<3}   train loss: mean {train_err.mean():.5f}, std: {train_err.std():.5f}   val loss: mean {val_err.mean():.5f}, std: {val_err.std():.5f}   time: {t_epoch:.2f} s")
-                train_err = []
-                val_err = []
-
-            # self.logger.console_logger.info(f"Model training epoch {i}")
 
 
     def generate_episodes(self):
         # fill a buffer using self.args.model_buffer_size
         self.logger.console_logger.info("Generating model based episodes")
 
-
-    # def train(self, batch: EpisodeBatch, t_env: int, episode_num: int):
-    #
-    #     # Get the relevant quantities
-    #     states = batch["state"][:, :-1]
-    #     actions = batch["actions_onehot"][:, :-1]
-    #     rewards = batch["reward"][:, :-1]
-    #
-    #     terminated = batch["terminated"][:, :-1].float()
-    #     mask = batch["filled"][:, :-1].float()
-    #     mask[:, 1:] = mask[:, 1:] * (1 - terminated[:, :-1])
-    #
-    #     # save tensors into temp dir
-    #     import os
-    #     tmp_dir = "tmp"
-    #     if not os.path.exists(tmp_dir):
-    #         os.mkdir("tmp")
-    #     for t, n in [(states, "states"), (actions, "actions"), (rewards, "rewards"), (terminated, "terminated")
-    #         , (mask, "mask")]:
-    #         th.save(t, os.path.join(tmp_dir, f"{n}_{episode_num:04}.pt"))
-
-
     def cuda(self):
         self.state_model.cuda()
+        self.obs_model.cuda()
--- src/modules/models/simple.py
+++ src/modules/models/simple.py
@@ -1,7 +1,12 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class SimPLeStateModel(nn.Module):
+    """
+
+    """
 
     def __init__(self, hidden_size, state_size, action_size, reward_size=1, term_size=1):
         super().__init__()
@@ -19,10 +24,33 @@ class SimPLeStateModel(nn.Module):
         self.rnn = nn.LSTMCell(hidden_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, output_size)
 
-    def forward(self, xt, ht, ct):
+    def forward(self, xt, ht_ct):
         xt = F.relu(self.fc1(xt))
-        ht, ct = self.rnn(xt, (ht, ct))
+        ht, ct = self.rnn(xt, ht_ct)
         yt = self.fc2(ht)
 
-        return yt, ht, ct
+        return yt, (ht, ct)
 
+    def init_hidden(self, batch_size, device):
+        ht = torch.zeros(batch_size, self.hidden_size).to(device)
+        ct = torch.zeros(batch_size, self.hidden_size).to(device)
+        return (ht, ct)
+
+
+class SimPLeObservationModel(nn.Module):
+    """
+
+    """
+    def __init__(self, input_size, output_size, hidden_size):
+        super().__init__()
+
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.rnn = nn.LSTM(hidden_size, hidden_size, batch_first=True)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        h, c = self.rnn(x)
+        y = self.fc2(h)
+
+        return y
